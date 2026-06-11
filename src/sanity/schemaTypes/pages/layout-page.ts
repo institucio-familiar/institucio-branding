9701f184f643879e1017e2465a7da46767b0cc4e
Alan <alandouglas96@gmail.com>
2026-06-11 21:46:31 +0900

    layout page schema

--- src/sanity/schemaTypes/pages/layout-page.ts
+++ src/sanity/schemaTypes/pages/layout-page.ts
@@ -1,4 +1,4 @@
-import { defineType, defineField } from 'sanity'
+import { defineType, defineField, defineArrayMember } from 'sanity'
 import { DocumentTextIcon } from '@sanity/icons'
 import { _mandatoryi18nField } from '@/sanity/lib/validations'
 import { badUsesField } from '@/sanity/schemaTypes/abtracts/bad-uses'
@@ -47,9 +47,115 @@ export const layoutPage = defineType({
           title: 'Descripción',
           type: 'i18n.text',
           validation: _mandatoryi18nField
+        }),
+        defineField({
+          name: 'overview',
+          title: 'Overview',
+          type: 'atom_1',
+          validation: (rule) => rule.required()
         })
       ]
-    })
-    // badUsesField('section5', 'Sección 5 - Malos usos')
+    }),
+    defineField({
+      name: 'section1',
+      title: 'Sección 1 (Sistema compositivo)',
+      type: 'object',
+      validation: (rule) => rule.required(),
+      fields: [
+        defineField({
+          name: 'title',
+          title: 'Título',
+          type: 'i18n.string',
+          validation: _mandatoryi18nField
+        }),
+        defineField({
+          name: 'description',
+          title: 'Descripción',
+          type: 'i18n.text',
+          validation: _mandatoryi18nField
+        }),
+        defineField({
+          name: 'configuration',
+          title: 'Configuración',
+          type: 'atom_2',
+          validation: (rule) => rule.required()
+        })
+      ]
+    }),
+    defineField({
+      name: 'section2',
+      title: 'Sección 2 (Formatos estándar)',
+      type: 'object',
+      validation: (rule) => rule.required(),
+      fields: [
+        defineField({
+          name: 'title',
+          title: 'Título',
+          type: 'i18n.string',
+          validation: _mandatoryi18nField
+        }),
+        defineField({
+          name: 'description',
+          title: 'Descripción',
+          type: 'i18n.text',
+          validation: _mandatoryi18nField
+        }),
+        defineField({
+          name: 'mediaAsset',
+          title: 'Media',
+          type: 'mediaAsset',
+          validation: (rule) => rule.required()
+        }),
+        defineField({
+          name: 'formats',
+          title: 'Formatos',
+          type: 'array',
+          of: [
+            defineArrayMember({
+              type: 'object',
+              fields: [
+                defineField({
+                  name: 'title',
+                  title: 'Título',
+                  type: 'i18n.string',
+                  validation: _mandatoryi18nField
+                }),
+                defineField({
+                  name: 'description',
+                  title: 'Descripción',
+                  type: 'i18n.text',
+                  validation: _mandatoryi18nField
+                }),
+                defineField({
+                  name: 'image1',
+                  title: 'Imagen 1 (Ejemplo)',
+                  type: 'mediaAsset',
+                  validation: (rule) => rule.required()
+                }),
+                defineField({
+                  name: 'image2',
+                  title: 'Imagen 2 (Grid)',
+                  type: 'mediaAsset',
+                  validation: (rule) => rule.required()
+                })
+              ]
+            })
+          ],
+          validation: (rule) => rule.required()
+        })
+      ],
+      preview: {
+        prepare() {
+          return { title: 'Sección 2 (Formatos estándar)' }
+        }
+      }
+    }),
+    defineField({
+      name: 'section3',
+      title: 'Sección 3 (Márgenes)',
+      type: 'atom_1',
+      validation: (rule) => rule.required()
+    }),
+    badUsesField('section4', 'Sección 4 - Malos usos')
   ]
 })
