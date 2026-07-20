4eb360aab9b312b9b95f069b9fa8d6c75351a4dd
Alan <alandouglas96@gmail.com>
2026-07-20 11:37:51 +0200

    Add asideSections slugs from titles

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+const SLUG_MAX_LENGTH = 96
+
+export function slugify(input: string): string {
+  return input
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+    .slice(0, SLUG_MAX_LENGTH)
+}
